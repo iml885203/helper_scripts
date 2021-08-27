fb26090ea6238ffc1d7496aa62a2efa5027b77aa
Long <iml885203@gmail.com>
2021-08-27 14:05:25 +0800

    Update twitch_auto_theatre.js

--- tampermonkey/twitch_auto_theatre.js
+++ tampermonkey/twitch_auto_theatre.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name         Twitch Auto Theatre
-// @name:zh-TW   Twitch 自動劇院模式
-// @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  auto click theatre mode button
-// @description:zh-tw 進入頁面自動啟動劇院模式
-// @author       Long
-// @match        https://www.twitch.tv/*
-// @icon         https://www.google.com/s2/favicons?domain=twitch.tv
-// @grant        none
+// @name               Twitch Auto Theatre
+// @name:zh-TW         Twitch 自動劇院模式
+// @namespace          http://tampermonkey.net/
+// @version            0.5
+// @description        auto click theatre mode button
+// @description:zh-tw  進入頁面自動啟動劇院模式
+// @author             Long
+// @match              https://www.twitch.tv/*
+// @icon               https://www.google.com/s2/favicons?domain=twitch.tv
+// @grant              none
 // ==/UserScript==
 
 (function () {
