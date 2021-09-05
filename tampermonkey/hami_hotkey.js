39da6a0c5998dfb0da6f7de8af9ee37ecd09a6ec
Long <iml885203@gmail.com>
2021-09-05 17:56:42 +0800

    Update hami_hotkey.js

--- tampermonkey/hami_hotkey.js
+++ tampermonkey/hami_hotkey.js
@@ -2,7 +2,7 @@
 // @name               Hami Hoykey
 // @name:zh-TW         Hami 快捷鍵
 // @namespace          http://tampermonkey.net/
-// @version            0.3
+// @version            0.4
 // @description        add hotkey for HamiVideo. f=fullscreen, Left/Right=TimeControl, space=play/pause, shift+>=speedup, shift+<=speeddown
 // @description:zh-tw  幫HamiVideo增加快捷鍵, f=全螢幕, Left/Right=時間控制, space=開始/暫停, shift+>=加速, shift+<=減速
 // @author             Long
@@ -20,7 +20,7 @@
       console.warn('[Hami Hotkey] fullscreen element not found.');
       return;
     }
-    if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+    if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) { // current working methods
       if ($fullScreenEle.requestFullscreen) {
         $fullScreenEle.requestFullscreen();
       } else if ($fullScreenEle.msRequestFullscreen) {
@@ -43,34 +43,45 @@
     }
   }
 
-  window.onload = function() {
-    var loop = setInterval(function () {
-      var $video = document.getElementById('h5video_html5_api');
-      // console.log($video);
-      if ($video) {
-        $video.addEventListener('keydown', (e) => {
-          // console.log(e);
-          if(e.key === 'ArrowLeft') {
-            $video.currentTime = Math.max(0, $video.currentTime - 5);
-          } else if(e.key === 'ArrowRight') {
-            $video.currentTime = Math.min($video.duration, $video.currentTime + 5);
-          } else if (e.key === ' ') {
-            if($video.paused) {
-              $video.play();
-            } else {
-              $video.pause();
-            }
-          } else if (e.shiftKey && e.key === '<') {
-            $video.playbackRate = Math.max(0.75, $video.playbackRate - 0.25);
-          } else if (e.shiftKey && e.key === '>') {
-            $video.playbackRate = Math.min(2, $video.playbackRate + 0.25);
-          } else if(e.key === 'f' || e.key === 'F') {
-            toggleFullscreen();
-          }
-        });
-        clearInterval(loop);
-        console.log("[Hami Hotkey] hotkey loaded.");
+  function initHotkey($video) {
+    $video.addEventListener('keydown', (e) => {
+      // console.log(e);
+      if(e.key === 'ArrowLeft') {
+        $video.currentTime = Math.max(0, $video.currentTime - 5);
+      } else if(e.key === 'ArrowRight') {
+        $video.currentTime = Math.min($video.duration, $video.currentTime + 5);
+      } else if (e.key === ' ') {
+        if($video.paused) {
+          $video.play();
+        } else {
+          $video.pause();
+        }
+      } else if (e.shiftKey && e.key === '<') {
+        $video.playbackRate = Math.max(0.75, $video.playbackRate - 0.25);
+      } else if (e.shiftKey && e.key === '>') {
+        $video.playbackRate = Math.min(2, $video.playbackRate + 0.25);
+      } else if(e.key === 'f' || e.key === 'F') {
+        toggleFullscreen();
       }
-    }, 500);
+    });
+    console.log("[Hami Hotkey] hotkey loaded.", $video);
   };
+
+  // handle dom created event
+  let observer = new MutationObserver(mutations => {
+    for(let mutation of mutations) {
+      for(let node of mutation.addedNodes) {
+        if (!(node instanceof HTMLElement)) continue;
+
+        if (node.matches('#h5video_html5_api')) {
+          initHotkey(node);
+        }
+      }
+    }
+  });
+  observer.observe(document.getElementById('player'), {
+    childList: true,
+    attributes: true,
+    attributeFilter: ['id']
+  });
 })();
