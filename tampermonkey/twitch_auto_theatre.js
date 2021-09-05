a22e324e02c1e32c10842b1581488bf8ce413bec
Long <iml885203@gmail.com>
2021-09-05 18:35:30 +0800

    Update twitch_auto_theatre.js

--- tampermonkey/twitch_auto_theatre.js
+++ tampermonkey/twitch_auto_theatre.js
@@ -2,7 +2,7 @@
 // @name               Twitch Auto Theatre
 // @name:zh-TW         Twitch 自動劇院模式
 // @namespace          http://tampermonkey.net/
-// @version            0.5
+// @version            0.6
 // @description        auto click theatre mode button
 // @description:zh-tw  進入頁面自動啟動劇院模式
 // @author             Long
@@ -14,20 +14,42 @@
 (function () {
   "use strict";
 
-  window.onload = function() {
-    var twitchAutoTheatreIntervalRetry = 100;
-    var twitchAutoTheatreInterval = setInterval(function () {
-      let $theatreModeButton = document.querySelector('[data-a-target="player-theatre-mode-button"]');
-      // console.log($theatreModeButton);
-      if ($theatreModeButton) {
-        $theatreModeButton.click();
-        clearInterval(twitchAutoTheatreInterval);
-        console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.");
-      } else if (!twitchAutoTheatreIntervalRetry) {
-        clearInterval(twitchAutoTheatreInterval);
-        console.warn("[Twitch-Auto-Theatre] theatre-mode-button not found.");
+  // method 1
+  // window.onload = function() {
+  //   var twitchAutoTheatreIntervalRetry = 100;
+  //   var twitchAutoTheatreInterval = setInterval(function () {
+  //     let $theatreModeButton = document.querySelector('[data-a-target="player-theatre-mode-button"]');
+  //     // console.log($theatreModeButton);
+  //     if ($theatreModeButton) {
+  //       $theatreModeButton.click();
+  //       clearInterval(twitchAutoTheatreInterval);
+  //       console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.");
+  //     } else if (!twitchAutoTheatreIntervalRetry) {
+  //       clearInterval(twitchAutoTheatreInterval);
+  //       console.warn("[Twitch-Auto-Theatre] theatre-mode-button not found.");
+  //     }
+  //     twitchAutoTheatreIntervalRetry--;
+  //   }, 500);
+  // };
+
+  // method 2
+  let observer = new MutationObserver(mutations => {
+    for(let mutation of mutations) {
+      let node = mutation.target;
+      if (node.matches('.player-controls__right-control-group')) {
+        observer.disconnect();
+        let $theatreModeButton = document.querySelector('[data-a-target="player-theatre-mode-button"]');
+        if ($theatreModeButton) {
+          $theatreModeButton.click();
+          console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.");
+        } else {
+          console.warn("[Twitch-Auto-Theatre] control-group found. but button not found.")
+        }
       }
-      twitchAutoTheatreIntervalRetry--;
-    }, 500);
-  };
+    }
+  });
+  observer.observe(document.getElementById('root'), {
+    childList: true,
+    subtree: true,
+  });
 })();
