3f7b55cfbd87cdbb0ee57839a7f5f85055eed922
Logan <iml885203@gmail.com>
2022-07-23 13:48:52 +0800

    Update twitch_auto_theatre.js

--- tampermonkey/twitch_auto_theatre.js
+++ tampermonkey/twitch_auto_theatre.js
@@ -2,7 +2,7 @@
 // @name               Twitch Auto Theatre
 // @name:zh-TW         Twitch 自動劇院模式
 // @namespace          http://tampermonkey.net/
-// @version            1.0.1
+// @version            1.0.2
 // @description        auto click theatre mode button
 // @description:zh-tw  進入頁面自動啟動劇院模式
 // @author             Long
@@ -14,28 +14,33 @@
 (function () {
   "use strict"
 
-  window.onload = function() {
-    let twitchAutoTheatreIntervalRetry = 300
-    const twitchAutoTheatreInterval = setInterval(function () {
-      const isEnabled = document.querySelector('.persistent-player').classList.contains('persistent-player--theatre')
-      if (isEnabled) {
-        clearInterval(twitchAutoTheatreInterval)
-        console.log("[Twitch-Auto-Theatre] theatre-mode enabled.")
-        return
-      }
+  let twitchAutoTheatreIntervalRetry = 300
+  const twitchAutoTheatreInterval = setInterval(function () {
+    if (!twitchAutoTheatreIntervalRetry) {
+      clearInterval(twitchAutoTheatreInterval)
+      console.warn("[Twitch-Auto-Theatre] theatre-mode-button not found.")
+      return
+    }
 
-      if (!twitchAutoTheatreIntervalRetry) {
-        clearInterval(twitchAutoTheatreInterval)
-        console.warn("[Twitch-Auto-Theatre] theatre-mode-button not found.")
-        return
-      }
-
-      const $theatreModeButton = document.querySelector('[data-a-target="player-theatre-mode-button"]')
-      if ($theatreModeButton) {
-        $theatreModeButton.click()
-        console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.")
-      }
+    const $persistentPlayer = document.querySelector('.persistent-player')
+    if (!$persistentPlayer) {
+      console.warn("[Twitch-Auto-Theatre] .persistent-player not found.")
       twitchAutoTheatreIntervalRetry--
-    }, 500)
-  }
+      return
+    }
+
+    const isEnabled = $persistentPlayer.classList.contains('persistent-player--theatre')
+    if (isEnabled) {
+      clearInterval(twitchAutoTheatreInterval)
+      console.log("[Twitch-Auto-Theatre] theatre-mode enabled.")
+      return
+    }
+
+    const $theatreModeButton = document.querySelector('[data-a-target="player-theatre-mode-button"]')
+    if ($theatreModeButton) {
+      $theatreModeButton.click()
+      console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.")
+    }
+    twitchAutoTheatreIntervalRetry--
+  }, 500)
 })()
