0cf965f88b8569a2000ddb8bce926ba89440e8c1
Logan <iml885203@gmail.com>
2023-12-09 21:59:24 +0800

    Update twitch_auto_theatre.js

--- tampermonkey/twitch_auto_theatre.js
+++ tampermonkey/twitch_auto_theatre.js
@@ -2,7 +2,7 @@
 // @name               Twitch Auto Theatre
 // @name:zh-TW         Twitch 自動劇院模式
 // @namespace          http://tampermonkey.net/
-// @version            1.0.2
+// @version            2023.12
 // @description        auto click theatre mode button
 // @description:zh-tw  進入頁面自動啟動劇院模式
 // @author             Long
@@ -12,35 +12,36 @@
 // ==/UserScript==
 
 (function () {
-  "use strict"
+    "use strict";
 
-  let twitchAutoTheatreIntervalRetry = 300
-  const twitchAutoTheatreInterval = setInterval(function () {
-    if (!twitchAutoTheatreIntervalRetry) {
-      clearInterval(twitchAutoTheatreInterval)
-      console.warn("[Twitch-Auto-Theatre] theatre-mode-button not found.")
-      return
-    }
+    let twitchAutoTheatreIntervalRetry = 300;
+    const twitchAutoTheatreInterval = setInterval(function () {
+        console.log("[Twitch-Auto-Theatre] try to enable theatre-mode...");
+        if (!twitchAutoTheatreIntervalRetry) {
+            clearInterval(twitchAutoTheatreInterval);
+            console.warn("[Twitch-Auto-Theatre] theatre-mode-button not found.");
+            return;
+        }
 
-    const $persistentPlayer = document.querySelector('.persistent-player')
-    if (!$persistentPlayer) {
-      console.warn("[Twitch-Auto-Theatre] .persistent-player not found.")
-      twitchAutoTheatreIntervalRetry--
-      return
-    }
+        const $persistentPlayer = document.querySelector(".persistent-player");
+        if (!$persistentPlayer) {
+            console.warn("[Twitch-Auto-Theatre] .persistent-player not found.");
+            twitchAutoTheatreIntervalRetry--;
+            return;
+        }
 
-    const isEnabled = $persistentPlayer.classList.contains('persistent-player--theatre')
-    if (isEnabled) {
-      clearInterval(twitchAutoTheatreInterval)
-      console.log("[Twitch-Auto-Theatre] theatre-mode enabled.")
-      return
-    }
+        const isEnabled = $persistentPlayer.classList.contains("persistent-player--theatre");
+        if (isEnabled) {
+            clearInterval(twitchAutoTheatreInterval);
+            console.log("[Twitch-Auto-Theatre] theatre-mode enabled.");
+            return;
+        }
 
-    const $theatreModeButton = document.querySelector('[data-a-target="player-theatre-mode-button"]')
-    if ($theatreModeButton) {
-      $theatreModeButton.click()
-      console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.")
-    }
-    twitchAutoTheatreIntervalRetry--
-  }, 500)
-})()
+        const $theatreModeButton = document.querySelector('[aria-label*="alt+t"]');
+        if ($theatreModeButton) {
+            $theatreModeButton.click();
+            console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.");
+        }
+        twitchAutoTheatreIntervalRetry--;
+    }, 500);
+})();
