171052999c22e2cc06777e6f08af274b62357f38
Logan <iml885203@gmail.com>
2024-04-28 21:33:16 +0800

    Update twitch_auto_theatre.js

--- tampermonkey/twitch_auto_theatre.js
+++ tampermonkey/twitch_auto_theatre.js
@@ -2,46 +2,83 @@
 // @name               Twitch Auto Theatre
 // @name:zh-TW         Twitch 自動劇院模式
 // @namespace          http://tampermonkey.net/
-// @version            1.0.3
+// @version            1.0.4
 // @description        auto click theatre mode button
 // @description:zh-tw  進入頁面自動啟動劇院模式
 // @author             Long
 // @match              https://www.twitch.tv/*
 // @icon               https://www.google.com/s2/favicons?domain=twitch.tv
 // @grant              none
+// @downloadURL https://update.greasyfork.org/scripts/431429/Twitch%20Auto%20Theatre.user.js
+// @updateURL https://update.greasyfork.org/scripts/431429/Twitch%20Auto%20Theatre.meta.js
 // ==/UserScript==
 
 (function () {
     "use strict";
 
-    let twitchAutoTheatreIntervalRetry = 300;
-    const twitchAutoTheatreInterval = setInterval(function () {
-        console.log("[Twitch-Auto-Theatre] try to enable theatre-mode...");
-        if (!twitchAutoTheatreIntervalRetry) {
-            clearInterval(twitchAutoTheatreInterval);
-            console.warn("[Twitch-Auto-Theatre] theatre-mode-button not found.");
-            return;
-        }
+    const ignorePathNames = [
+        '/directory',
+    ]
+    const isPathIgnored = (pathname) => ignorePathNames.some(ignoredPath => pathname.startsWith(ignoredPath));
 
-        const $persistentPlayer = document.querySelector(".persistent-player");
-        if (!$persistentPlayer) {
-            console.warn("[Twitch-Auto-Theatre] .persistent-player not found.");
-            twitchAutoTheatreIntervalRetry--;
-            return;
-        }
+    const stopAutoTheatreInterval = () => {
+        if (!window.autoTheatreInterval) return;
+        clearInterval(window.autoTheatreInterval);
+        window.autoTheatreInterval = null;
+    }
 
-        const isEnabled = $persistentPlayer.classList.contains("persistent-player--theatre");
-        if (isEnabled) {
-            clearInterval(twitchAutoTheatreInterval);
-            console.log("[Twitch-Auto-Theatre] theatre-mode enabled.");
-            return;
-        }
+    const autoTheatre = () => {
+        let retry = 300;
+        window.autoTheatreInterval = setInterval(function () {
+            console.log("[Twitch-Auto-Theatre] try to enable theatre-mode...");
+            if (!retry) {
+                stopAutoTheatreInterval();
+                console.warn("[Twitch-Auto-Theatre] theatre-mode-button not found.");
+                return;
+            }
 
-        const $theatreModeButton = document.querySelector('[aria-label*="alt+t"]');
-        if ($theatreModeButton) {
-            $theatreModeButton.click();
-            console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.");
-        }
-        twitchAutoTheatreIntervalRetry--;
-    }, 500);
+            const $persistentPlayer = document.querySelector(".persistent-player");
+            if (!$persistentPlayer) {
+                console.warn("[Twitch-Auto-Theatre] .persistent-player not found.");
+                retry--;
+                return;
+            }
+
+            const isEnabled = $persistentPlayer.classList.contains("persistent-player--theatre");
+            if (isEnabled) {
+                stopAutoTheatreInterval();
+                console.log("[Twitch-Auto-Theatre] theatre-mode enabled.");
+                return;
+            }
+
+            const $theatreModeButton = document.querySelector('[aria-label*="alt+t"]');
+            if ($theatreModeButton) {
+                $theatreModeButton.click();
+                console.log("[Twitch-Auto-Theatre] theatre-mode-button clicked.");
+            }
+            retry--;
+        }, 500);
+    }
+
+    const handlePageLoaded = () => {
+        if (isPathIgnored(location.pathname)) return;
+        autoTheatre();
+    }
+
+    const handlePageChanged = () => {
+        window.navigation.addEventListener("navigate", (event) => {
+            window.gg = event;
+            console.log('location changed!', event);
+            const url = new URL(event.destination.url);
+            if (window.autoTheatreInterval) stopAutoTheatreInterval();
+            if (isPathIgnored(url.pathname)) return;
+            autoTheatre();
+        });
+    }
+
+    const main = () => {
+        handlePageChanged();
+        handlePageLoaded();
+    }
+    main();
 })();
