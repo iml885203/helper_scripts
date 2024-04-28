88491157cfc93038de51d35a8f9b96527fc4c9e3
Logan <iml885203@gmail.com>
2024-04-28 22:06:25 +0800

    Update twitch_auto_channel_points.js

--- tampermonkey/twitch_auto_channel_points.js
+++ tampermonkey/twitch_auto_channel_points.js
@@ -2,7 +2,7 @@
 // @name               Twitch Auto Channel Points
 // @name:zh-TW         Twitch 自動獲得忠誠點數
 // @namespace          http://tampermonkey.net/
-// @version            1.4
+// @version            1.5
 // @description        auto get Channel Points
 // @description:zh-tw  自動獲得忠誠點數
 // @author             Long
@@ -11,130 +11,167 @@
 // @grant              GM_addStyle
 // @grant              GM_setValue
 // @grant              GM_getValue
+// @downloadURL https://update.greasyfork.org/scripts/435447/Twitch%20Auto%20Channel%20Points.user.js
+// @updateURL https://update.greasyfork.org/scripts/435447/Twitch%20Auto%20Channel%20Points.meta.js
 // ==/UserScript==
 
 (function () {
-  "use strict";
-
-  GM_addStyle(`
-    #total-got-points {
-      display: inline-flex;
-      position: relative;
-      align-items: center;
-      padding : 0 var(--button-padding-x) 0 var(--button-padding-x);
-      text-decoration: none;
-      white-space: nowrap;
-      user-select: none;
-      color: var(--color-text-alt-2);
-      font-weight: var(--font-weight-semibold);
-      border-radius: var(--border-radius-medium);
-      font-size: var(--button-text-default);
-      height: var(--button-size-default);
-      --point-name: '自動領取忠誠點數';
-    }
-
-    #total-got-points:hover {
-      background-color: var(--color-background-button-text-hover);
-    }
-
-    #total-got-points img {
-      height: 2rem;
-      width: 2rem;
-    }
-
-    #total-got-points img, #total-got-points svg {
-      margin-right: 5px;
-    }
-
-    #total-got-points::before{
-      content: var(--point-name);
-      user-select: none;
-      color: var(--color-text-tooltip);
-      background-color: var(--color-background-tooltip);
-      font-size: var(--font-size-6);
-      border-radius: 0.4rem;
-      padding: 0.5rem;
-      max-width: 30rem;
-      font-weight: var(--font-weight-semibold);
-      right: 0;
-      line-height: 1.2;
-      pointer-events: none;
-      position: absolute;
-      text-align: left;
-      white-space: nowrap;
-      z-index: 2000;
-      top: 50%;
-      transform: translate(calc(100% + 5px), -50%);
-      transition: all .2s ease-in-out;
-      opacity: 0;
-    }
-
-    #total-got-points:hover::before{
-      opacity: 1;
-    }
-  `);
-
-  // find button
-  let findButtonRetry = 100;
-  // get points
-  let getPointsLoop = 1000;
-  let pointsOneTime = 50;
-  let GMkeyName = location.origin + location.pathname + ':auto-channel-points';
-  let totalGotPoints = GM_getValue(GMkeyName, 0);
-  let beforePoints = 0;
-  console.log("[Twitch-Auto-Channel-Points] totalGotPoints: ", totalGotPoints);
-
-
-  let getPoints = function() {
-    // console.debug('[Twitch-Auto-Channel-Points] get points');
-    let $pointsSummaryButtons = document.querySelectorAll('[data-test-selector="community-points-summary"] button');
-    let canGetPoints = $pointsSummaryButtons.length > 1;
-    if(canGetPoints) {
-      beforePoints = document.querySelector('.chat-input__buttons-container button').innerText;
-      $pointsSummaryButtons[1].click();
-      $pointsSummaryButtons[1].remove();
-      totalGotPoints += pointsOneTime;
-      GM_setValue(GMkeyName, totalGotPoints);
-      showTotalGotPoints();
-      console.log("[Twitch-Auto-Channel-Points] Got points!!");
-      console.log("[Twitch-Auto-Channel-Points] totalGotPoints: ", totalGotPoints);
-    }
-  };
-
-  let showTotalGotPoints = function() {
-    let $totalGotPoints = document.querySelector('#total-got-points span');
-    if(!$totalGotPoints) {
-      let $totalGotPointsContainer = document.createElement('div');
-      $totalGotPointsContainer.id = 'total-got-points';
-      let $buttonDiv = document.querySelectorAll('.chat-input__buttons-container div')[0];
-      $buttonDiv.appendChild($totalGotPointsContainer);
-      let $pointImage = document.querySelector('.channel-points-icon__image');
-      if($pointImage) {
-        let pointName = $pointImage.alt;
-        $totalGotPointsContainer.style.setProperty('--point-name', '"自動領取' + pointName + '"');
-        $totalGotPointsContainer.appendChild($pointImage.cloneNode(true));
-      } else {
-        $totalGotPointsContainer.appendChild(document.querySelector('.chat-input__buttons-container button svg').cloneNode(true));;
+    "use strict";
+  
+    GM_addStyle(`
+      #total-got-points {
+        display: inline-flex;
+        position: relative;
+        align-items: center;
+        padding : 0 var(--button-padding-x) 0 var(--button-padding-x);
+        text-decoration: none;
+        white-space: nowrap;
+        user-select: none;
+        color: var(--color-text-alt-2);
+        font-weight: var(--font-weight-semibold);
+        border-radius: var(--border-radius-medium);
+        font-size: var(--button-text-default);
+        height: var(--button-size-default);
+        --point-name: '自動領取忠誠點數';
       }
-      $totalGotPointsContainer.appendChild(document.createElement('span'));
-      $totalGotPoints = document.querySelector('#total-got-points span');
-    }
-    $totalGotPoints.textContent = new Intl.NumberFormat().format(totalGotPoints);;
-  };
+  
+      #total-got-points:hover {
+        background-color: var(--color-background-button-text-hover);
+      }
+  
+      #total-got-points img {
+        height: 2rem;
+        width: 2rem;
+      }
+  
+      #total-got-points img, #total-got-points svg {
+        margin-right: 5px;
+      }
+  
+      #total-got-points::before{
+        content: var(--point-name);
+        user-select: none;
+        color: var(--color-text-tooltip);
+        background-color: var(--color-background-tooltip);
+        font-size: var(--font-size-6);
+        border-radius: 0.4rem;
+        padding: 0.5rem;
+        max-width: 30rem;
+        font-weight: var(--font-weight-semibold);
+        right: 0;
+        line-height: 1.2;
+        pointer-events: none;
+        position: absolute;
+        text-align: left;
+        white-space: nowrap;
+        z-index: 2000;
+        top: 50%;
+        transform: translate(calc(100% + 5px), -50%);
+        transition: all .2s ease-in-out;
+        opacity: 0;
+      }
+  
+      #total-got-points:hover::before{
+        opacity: 1;
+      }
+    `);
 
-  let findButtonInterval = setInterval(function () {
-    // console.debug('[Twitch-Auto-Channel-Points] find button...');
-    let $pointButton = document.querySelector('[data-test-selector="community-points-summary"] button');
-    if ($pointButton) {
-      setInterval(getPoints, getPointsLoop);
-      showTotalGotPoints();
-      clearInterval(findButtonInterval);
-      console.log("[Twitch-Auto-Channel-Points] found point-button. start get points");
-    } else if (!findButtonRetry) {
-      clearInterval(findButtonInterval);
-      console.warn("[Twitch-Auto-Channel-Points] point-button not found.");
+    const ignorePathNames = [
+        '/directory',
+    ]
+    const isPathIgnored = (pathname) => ignorePathNames.some(ignoredPath => pathname.startsWith(ignoredPath));
+
+    // find button
+    let findButtonRetry = 100;
+    // get points
+    const getPointsLoop = 1000;
+    const pointsOneTime = 50;
+    
+    const startGetPointsLoop = () => {
+        const GMkeyName = location.origin + location.pathname + ':auto-channel-points';
+        const totalGotPoints = GM_getValue(GMkeyName, 0);
+        showTotalGotPoints(totalGotPoints);
+        window.getPointLoop = setInterval(function() {
+            const $pointsSummaryButtons = document.querySelectorAll('[data-test-selector="community-points-summary"] button');
+            const canGetPoints = $pointsSummaryButtons.length > 1;
+            if(canGetPoints) {
+                $pointsSummaryButtons[1].click();
+                $pointsSummaryButtons[1].remove();
+                totalGotPoints += pointsOneTime;
+                GM_setValue(GMkeyName, totalGotPoints);
+                console.log("[Twitch-Auto-Channel-Points] Got points!!");
+                showTotalGotPoints(totalGotPoints);
+            }
+        }, getPointsLoop);
     }
-    findButtonRetry--;
-  }, 500);
+    const stopGetPointsLoop = () => {
+        if (!window.getPointLoop) return;
+        clearInterval(window.getPointLoop);
+        window.getPointLoop = null;
+    }
+
+    const showTotalGotPoints = function(totalGotPoints) {
+        const $totalGotPoints = document.querySelector('#total-got-points span');
+        if(!$totalGotPoints) {
+            const $totalGotPointsContainer = document.createElement('div');
+            $totalGotPointsContainer.id = 'total-got-points';
+            const $buttonDiv = document.querySelectorAll('.chat-input__buttons-container div')[0];
+            $buttonDiv.appendChild($totalGotPointsContainer);
+            const $pointImage = document.querySelector('.channel-points-icon__image');
+            if($pointImage) {
+                const pointName = $pointImage.alt;
+                $totalGotPointsContainer.style.setProperty('--point-name', '"自動領取' + pointName + '"');
+                $totalGotPointsContainer.appendChild($pointImage.cloneNode(true));
+            } else {
+                $totalGotPointsContainer
+                    .appendChild(document.querySelector('.chat-input__buttons-container button svg')
+                    .cloneNode(true));
+            }
+            $totalGotPointsContainer.appendChild(document.createElement('span'));
+            $totalGotPoints = document.querySelector('#total-got-points span');
+        }
+        $totalGotPoints.textContent = new Intl.NumberFormat().format(totalGotPoints);
+        console.log("[Twitch-Auto-Channel-Points] totalGotPoints: ", totalGotPoints);
+    };
+
+    const startFindPointButtonLoop = () => {
+        window.findPointButtonLoop = setInterval(function () {
+            console.log('[Twitch-Auto-Channel-Points] find point-button...');
+            let $pointButton = document.querySelector('[data-test-selector="community-points-summary"] button');
+            if ($pointButton) {
+                startGetPointsLoop();
+                stopFindPointButtonLoop();
+                console.log("[Twitch-Auto-Channel-Points] found point-button. start auto get points");
+            } else if (!findButtonRetry) {
+                stopFindPointButtonLoop();
+                console.warn("[Twitch-Auto-Channel-Points] point-button not found.");
+            }
+            findButtonRetry--;
+        }, 500);
+    };
+    const stopFindPointButtonLoop = () => {
+        if (!window.findPointButtonLoop) return;
+        clearInterval(window.findPointButtonLoop);
+        window.findPointButtonLoop = null;
+    }
+    
+    const handlePageLoaded = (pathname) => {
+        if (isPathIgnored(pathname)) return;
+        startFindPointButtonLoop();
+    }
+    const handlePageChanged = () => {
+        window.navigation.addEventListener("navigate", (event) => {
+            stopFindPointButtonLoop();
+            stopGetPointsLoop();
+
+            const url = new URL(event.destination.url);
+            handlePageLoaded(url.pathname);
+        });
+    }
+
+    const main = () => {
+        handlePageLoaded(location.pathname);
+        handlePageChanged();
+    }
+    main();
 })();
-
